58ac215c93d371a5319b9f31b68b9ab43ba144e0
GonzaloSantizo <san21504@uvg.edu.gt>
2023-08-25 19:40:29 -0600

    Final changes to original file

--- DPLL.py
+++ DPLL.py
@@ -1,7 +1,22 @@
+def parse_literal(literal_str):
+    if literal_str.startswith("¬"):
+        variable = literal_str[1:]
+        value = False
+    else:
+        variable = literal_str
+        value = True
+    return variable, value
+
+def parse_clause(clause_str):
+    literals = clause_str.split(",")
+    return [parse_literal(literal.strip()) for literal in literals]
+
+def parse_formula(formula_str):
+    clauses = formula_str.split(";")
+    return [parse_clause(clause.strip()) for clause in clauses]
+
 def evaluate_clause(clause, assignment):
-    for literal in clause:
-        variable = abs(literal)
-        value = literal > 0  # True if positive, False if negative
+    for variable, value in clause:
         if variable not in assignment:
             continue
         if assignment[variable] == value:
@@ -15,19 +30,26 @@ def evaluate_formula(formula, assignment):
     return True
 
 def brute_force_satisfiability(formula):
-    num_variables = max(abs(literal) for clause in formula for literal in clause)
+    variables = set()
+    for clause in formula:
+        for variable, _ in clause:
+            variables.add(variable)
+    num_variables = len(variables)
+
+    variable_list = list(variables)
+    variable_index_map = {variable: index for index, variable in enumerate(variable_list)}
+
     for i in range(2 ** num_variables):
         assignment = {}
-        for j in range(num_variables):
-            assignment[j + 1] = bool((i >> j) & 1)
+        for j, variable in enumerate(variable_list):
+            assignment[variable] = bool((i >> j) & 1)
         if evaluate_formula(formula, assignment):
             return True, assignment
     return False, None
 
-# Example input: {{p}, {p}}
-formula = [
-    [1], [1]
-]
+# Example input: "{{p, q}, {q, s}, {p, s}, {q, s}}"
+formula_str = "{{p, q}, {q, s}, {p, s}, {q, s}}"
+formula = parse_formula(formula_str)
 
 satisfiable, assignment = brute_force_satisfiability(formula)
 if satisfiable:
